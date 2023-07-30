e9dfccf9222252b1f889dbbf359f40ef902bc6c5
Diogo Mendonça <diogomendonca2015@outlook.com.br>
2023-07-29 23:31:49 -0300

    Adição do método calcularTranposta(); Modificação do método imprimirMatriz() para toString(); Método calcularDeterminante() a ser implementado

---
+++ src/main/java/br/ufpb/dcx/diogo/matrizes/Matriz.java
@@ -0,0 +1,66 @@
+package br.ufpb.dcx.diogo.matrizes;
+
+public class Matriz {
+
+    private int linhas;
+    private int colunas;
+    private int[][] matriz;
+
+    public Matriz(int linhas, int colunas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.matriz = new int[linhas][colunas];
+    }
+
+    public int getElemento(int linha, int coluna) {
+        return matriz[linha][coluna];
+    }
+
+    public void setElemento(int linha, int coluna, int elemento) {
+        matriz[linha][coluna] = elemento;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder matrizString = new StringBuilder();
+        for (int[] linha : this.matriz) {
+            matrizString.append("[ ");
+            for (int elemento : linha) {
+                matrizString.append(String.format("%3d", elemento)).append(" ");
+            }
+            matrizString.append("  ]\n");
+        }
+        return matrizString.toString();
+    }
+
+    public Matriz calcularTransposta() {
+        Matriz transposta = new Matriz(colunas, linhas);
+        for (int i = 0; i < colunas; i++) {
+            for (int j = 0; j < linhas; j++) {
+                int elemento = matriz[j][i];
+                transposta.setElemento(i, j, elemento);
+            }
+
+        }
+        return transposta;
+    }
+
+    public int calcularDeterminante() throws IllegalArgumentException {
+        if (linhas != colunas) {
+            throw new IllegalArgumentException("Só é possível calcular o determinante de matrizes quadradas.");
+        }
+
+        int n = linhas = colunas;
+        int somatorio = 0;
+        if (n == 1) {
+            return matriz[0][0];
+        } else if (n == 2) {
+            return matriz[0][0] * matriz[1][1] - matriz[1][0] * matriz[0][1];
+        } else {
+            //TODO
+        }
+        return 0;
+    }
+
+}
--- src/main/java/br/ufpb/dcx/diogo/matrizes/TestaMatriz.java
+++ src/main/java/br/ufpb/dcx/diogo/matrizes/TestaMatriz.java
@@ -7,25 +7,30 @@ public class TestaMatriz {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Digite a quantidade de linhas: ");
-        int linhas = entrada.nextInt();
+        try {
+            System.out.print("Digite a quantidade de linhas: ");
+            int linhas = entrada.nextInt();
 
-        System.out.print("Digite a quantidade de colunas: ");
-        int colunas = entrada.nextInt();
+            System.out.print("Digite a quantidade de colunas: ");
+            int colunas = entrada.nextInt();
 
-        GeradorMatrizes geradorMatrizes = new GeradorMatrizes(linhas, colunas);
-        int[][] matriz = geradorMatrizes.criarMatriz();
+            Matriz matriz = new Matriz(linhas, colunas);
 
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
-                System.out.printf("Digite o elemento da %sª linha e %sª coluna: ", i+1, j+1);
-                int elemento = entrada.nextInt();
-                matriz[i][j] = elemento;
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    System.out.printf("Digite o elemento da %sª linha e %sª coluna: ", i + 1, j + 1);
+                    int elemento = entrada.nextInt();
+                    matriz.setElemento(i, j, elemento);
+                }
             }
+            System.out.println();
+            System.out.println(matriz);
+            System.out.println(matriz.calcularDeterminante());
+
+            entrada.close();
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
         }
-        System.out.println();
-        System.out.println(geradorMatrizes.imprimirMatriz(matriz));
-        entrada.close();
     }
 
 }
